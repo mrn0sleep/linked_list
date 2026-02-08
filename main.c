069d7750776f11b351f53f8404243c775b5f814a
chokorimu <rezondavar@gmail.com>
2026-02-08 13:24:29 +0700

    clean

--- main.c
+++ main.c
@@ -5,13 +5,6 @@
 int main() {
     struct node* head = NULL;
     DynamicArray arr;
-    initArray(&arr,5);
-    addArray(&arr,1);
-    addArray(&arr,2);
-    addArray(&arr,3);
-    addArray(&arr,4);
-    addArray(&arr,5);
-    iterateArray(&arr);
 
     return 0;
 }
